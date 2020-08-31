e5faf1f15b31f3d67bbee5c4677eedc042684aac
Ingrid Carlson <ingridclaire@gmail.com>
2020-08-31 14:45:24 -0700

    implement main sort function for mergeSort algorithm

--- sortingAlgos/mergeSort.js
+++ sortingAlgos/mergeSort.js
@@ -1,33 +1,43 @@
 const mergeSorted = (arr1, arr2) => {
-  // set an empty array to collect merged items
   let merged = [];
-  // set tracker for arr1 index
   let left = 0;
-  // set tracker for arr2 index
   let right = 0;
-  // run loop while index of both is less than length of both
   while (left < arr1.length && right < arr2.length) {
-    // compare arr1[index] to arr2[index]
     if (arr1[left] <= arr2[right]) {
-      // push the smaller one into result array
       merged.push(arr1[left]);
-      // increment corresponding index
       left++;
     } else {
       merged.push(arr2[right]);
       right++;
     }
   }
-  // check if one of the arrays has remaining elements
   if (left === arr1.length) {
-    // if one does, concat it to the result array and return
     return merged.concat(arr2.slice(right));
-  } else if (right === arr2.length) {
+  }
+  if (right === arr2.length) {
     return merged.concat(arr1.slice(left));
   }
 }
 
-let first = [1, 10, 50, 60, 70, 110];
+const sort = (array) => {
+  // base case: check if array's length is <= 1
+    // if yes, return array;
+  if (array.length <= 1) {
+    return array;
+  }
+  // set a midpoint variable
+  let midpoint = Math.floor(array.length / 2);
+  // split array in half using slice and midpoint;
+  let first = array.slice(0, midpoint);
+  let second = array.slice(midpoint);
+  console.log(`--------------\n${first}\n${second}\n----------------`)
+  // return: merge the two arrays after they've been passed back into sort
+  return mergeSorted(sort(first), sort(second));
+}
+
+let first = [75, 1, 99, 32, 10, 3, 18, -1, 55];
 let second = [-134, -2, 0, 14, 50, 77, 84, 98, 99, 100];
 
-console.log(mergeSorted(first, second));
+// console.log(mergeSorted(first, second));
+
+console.log(sort(first));
